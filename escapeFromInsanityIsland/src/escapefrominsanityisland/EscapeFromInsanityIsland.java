fa4434f2239ce0515169b75bed68d6c53ad29cdd
Christopher <Christopher@192.168.0.2>
2016-05-10 20:33:10 -0600

    5/10/16 - Initial commit to repository

--- escapeFromInsanityIsland/src/escapefrominsanityisland/EscapeFromInsanityIsland.java
+++ escapeFromInsanityIsland/src/escapefrominsanityisland/EscapeFromInsanityIsland.java
@@ -5,17 +5,27 @@
  */
 package escapefrominsanityisland;
 
+import byui.cit260.escapeFromInsanityIsland.model.Player;
+
 /**
  *
- * @author Christopher and Andre
+ * @authors Christopher and Andre
  */
 public class EscapeFromInsanityIsland {
 
+    //class instance variables
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Player playerOne = new Player();
+        
+        playerOne.setName("Fred");
+        playerOne.setBestTime(7.00);
+        
+        String playerInfo = playerOne.toString();
+        System.out.println(playerInfo);
     }
     
 }
