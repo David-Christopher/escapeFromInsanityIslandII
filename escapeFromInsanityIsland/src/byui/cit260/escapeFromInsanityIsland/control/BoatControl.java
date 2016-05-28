024e72fa6509f9d51e91d2b650e91999db8d4ac7
Christopher <Christopher@192.168.0.2>
2016-05-27 20:59:56 -0600

    5/27 Individual Assignment

---
+++ escapeFromInsanityIsland/src/byui/cit260/escapeFromInsanityIsland/control/BoatControl.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.escapeFromInsanityIsland.control;
+
+/**
+ *
+ * @author Christopher
+ */
+public class BoatControl {
+    public double calcTreesToMoveBoat(double length, double radius) {
+       if(length < 126) {
+            return -1;
+        }
+        if(radius < 4 || radius > 5) {
+            return -1;
+        }
+        double treeRollers = 198 / (Math.pow(radius, 2)) * .333;
+            return treeRollers;
+    }
+}
