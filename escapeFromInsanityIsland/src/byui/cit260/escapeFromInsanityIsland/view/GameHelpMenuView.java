19a905f34713a6c32205e2047c4b8c000c7746d3
Christopher <Christopher@192.168.0.2>
2016-06-03 23:27:51 -0600

    6/3/16 UPDATE - With some menu details.

--- escapeFromInsanityIsland/src/byui/cit260/escapeFromInsanityIsland/view/GameHelpMenuView.java
+++ escapeFromInsanityIsland/src/byui/cit260/escapeFromInsanityIsland/view/GameHelpMenuView.java
@@ -5,12 +5,124 @@
  */
 package byui.cit260.escapeFromInsanityIsland.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Christopher
  */
 public class GameHelpMenuView {
-    void displayHelpMenu() {
-        System.out.println("\n*** displayHelpMenu stub finction called ***");
+    
+    private String helpMenu;
+    private String promptMessage = "Enter your selection below";
+    
+    public GameHelpMenuView() {
+        this.helpMenu = "\n"
+                  + "\n------------------------------------------------"
+                  + "\n| Help Menu                                    |"
+                  + "\n------------------------------------------------"
+                  + "\nG - What Is The Goal of The Game?"    
+                  + "\nM - How to Move?" 
+                  + "\nT - Tools Available to Construct"
+                  + "\nC - Constructing Tools"
+                  + "\nV - View Map"
+                  + "\nA - Avoiding Traps/Beasts"
+                  + "\nQ - Quit the game"
+                  + "\n------------------------------------------------";
+    }
+
+    public void displayHelpMenu() {
+        boolean done = false; // set flag to not done
+        do {
+            System.out.println(helpMenu);
+            //prompt for and get the players name
+            String helpMenuOption = this.getHelpMenuOption();
+            if (helpMenuOption.toUpperCase().equals("Q")) // user wants to quit
+                return; // Take them back a screen
+            
+            // do the requested action and display the next view
+            done = this.doAction(helpMenuOption);
+            
+        } while (!done);
+    }
+
+
+
+    private String getHelpMenuOption() {
+        
+        Scanner keyboard = new Scanner(System.in); // get infile for keyboard
+        String value = ""; // value to be returned
+        boolean valid = false; // initialize to not valid
+
+        while (!valid) { // loop whilean invalid value is entered
+            System.out.println("\n" + this.promptMessage);
+            
+            value = keyboard.nextLine(); // get next line typed on keyboard
+            value = value.trim(); // trim off leading and trailing blanks
+
+            if (value.length() < 1) { // value is blank
+                System.out.println("\nInvalid value: value can not be blank");
+                continue;
+            }
+            
+            break; // end the loop
+               
+        }
+        return value; // return the value entered
+                        
+    }
+
+    private boolean doAction(String choice) {
+        choice.toUpperCase(); // convert choice to upper case
+        
+        switch (choice) {
+            case "G": // create and start a new game
+                this.seeGoal();
+                break;   
+            case "M": // get and start an existing game
+                this.howToMove();
+                break;
+            case "T": // display help menu
+                this.toolsAvailable();
+                break;
+            case "C": // save the game
+                this.constructingTools();
+                break;
+            case "V": // display help menu
+                this.viewMap();
+                break;
+            case "A": // save the game
+                this.avoidingTrapsBeasts();
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+                break;
+        }
+        return false; 
+    }
+    
+    private void seeGoal() {
+           HelpMenuItems seeGoal = new HelpMenuItems();
+        seeGoal.displaySeeGoal();     
+    }
+        
+    private void howToMove(){
+        System.out.println("\n*** howToMove() function called ***");
+    }
+
+    private void toolsAvailable() {
+        System.out.println("\n*** toolsAvailable() function called ***");
+    }
+
+    private void constructingTools() {
+        System.out.println("\n*** constructingTools() function called ***");  
+    }
+    
+    private void viewMap() {
+        System.out.println("\n*** viewMap() function called ***");
+    }
+
+    private void avoidingTrapsBeasts() {
+        System.out.println("\n*** avoidingTrapsBeasts() function called ***");  
     }
 }
---
+++ escapeFromInsanityIsland/src/byui/cit260/escapeFromInsanityIsland/view/HelpMenuItems.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.escapeFromInsanityIsland.view;
+
+
+/**
+ *
+ * @author Christopher
+ */
+public class HelpMenuItems {
+       
+public void displaySeeGoal() {
+    System.out.println (
+                "In order to progress, you must choose to explore the island, find resources\n" +
+                "to make tools, medicine, weapons, armor, and find a way to escape the island.\n" +
+                "There are traps and beasts prowling the jungle that you will need to avoid.\n" +
+                "While searching for a way out, clues will be given like, foot patterns to \n" +
+                "follow, bent weeds and grass to give you a hint of where to go, providing more \n" +
+                "ideas as to who you are and how you got there. When you make it to the end of \n" +
+                "the island, a boat can be made with the scraps that were found. You will then\n" +
+                "be able to escape from Insanity Island.");
+               
+    }
+
+}
